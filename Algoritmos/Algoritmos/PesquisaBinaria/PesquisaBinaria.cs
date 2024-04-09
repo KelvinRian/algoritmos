6703be826c4c9a11261b829e9d3d449dd7c4d28f
KelvinRian <kelvinlourenco18@gmail.com>
2024-04-09 13:01:39 -0300

    Criado teste para pesquisa binaria retornar itens existentes

---
+++ Algoritmos/Algoritmos/PesquisaBinaria/PesquisaBinaria.cs
@@ -0,0 +1,10 @@
+﻿namespace Algoritmos.PesquisaBinaria
+{
+    public static class PesquisaBinaria
+    {
+        public static int? Pesquisar(IList<int> lista, int item)
+        {
+            return null;
+        }
+    }
+}
---
+++ Algoritmos/Testes/PesquisaBinariaTestes/PesquisaBinariaTestes.cs
@@ -0,0 +1,17 @@
+﻿using Algoritmos.PesquisaBinaria;
+
+namespace Testes.PesquisaBinariaTestes
+{
+    public class PesquisaBinariaTestes
+    {
+        [Fact]
+        public void Deve_retornar_item_existente()
+        {
+            var lista = new List<int>() { 1, 3, 5, 7, 9 };
+            var item = 3;
+
+            var resultado = PesquisaBinaria.Pesquisar(lista, item);
+            Assert.Equal(item, resultado);
+        }
+    }
+}
