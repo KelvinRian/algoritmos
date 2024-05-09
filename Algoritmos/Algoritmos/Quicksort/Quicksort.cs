42655c3b55df2b9b1f25083439ef783171ce175f
KelvinRian <kelvinlourenco18@gmail.com>
2024-05-09 11:29:36 -0300

    Quickstort

---
+++ Algoritmos/Algoritmos/Quicksort/Quicksort.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace Algoritmos.Quicksort
+{
+    public static class Quicksort
+    {
+        public static IList<int> QuickSort(IList<int> list)
+        {
+            if (list == null)
+            {
+                return new List<int>();
+            }
+            else if (list.Count < 2)
+            {
+                return list;
+            }
+            else
+            {
+                var pivo = list[0];
+                var menores = list.Where(x => x < pivo);
+                var maiores = list.Where(x => x > pivo);
+
+                var menoresOrdenados = QuickSort(menores.ToList());
+                var maioresOrdenados = QuickSort(maiores.ToList());
+
+                var novaLista = menoresOrdenados.Append(pivo);
+                novaLista = novaLista.Concat(maioresOrdenados);
+
+                return novaLista.ToList();
+            }
+        }
+    }
+}
---
+++ Algoritmos/Testes/QuicksortTestes/QuicksortTestes.cs
@@ -0,0 +1,29 @@
+﻿using Algoritmos.Quicksort;
+
+namespace Testes.QuicksortTestes
+{
+    public class QuicksortTestes
+    {
+        [Fact]
+        public void QuickSortDeveRetornarCasoBase()
+        {
+            var lista = new List<int>();
+            var resultado = Quicksort.QuickSort(lista);
+
+            Assert.Equal(lista, resultado);
+        }
+
+        [Fact]
+        public void QuickSortDeveOrdenarLista()
+        {
+            var lista = new List<int> { 3, 5, 1, 4, 2};
+            var resultado = Quicksort.QuickSort(lista);
+
+            Assert.Equal(1, resultado[0]);
+            Assert.Equal(2, resultado[1]);
+            Assert.Equal(3, resultado[2]);
+            Assert.Equal(4, resultado[3]);
+            Assert.Equal(5, resultado[4]);
+        }
+    }
+}
